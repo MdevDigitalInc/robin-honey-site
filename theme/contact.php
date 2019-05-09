0944566d023e9a21d465883d148e7efd86924591
Dorian <dorian@mdev.digital>
2019-05-09 11:37:28 -0400

    adjustments to about and work page templates

--- theme/contact.php
+++ theme/contact.php
@@ -1,15 +1,7 @@
 <section class="rhd-section-blue" id="contact">
-
-    <div class="rhd-container">
-
-
-
-      <h1>Get in touch</h1>
-
-      <p>Provide some friendly text here, what kind of things do you want people contacting you for?</p>
-
-      <?php echo do_shortcode( '[contact-form-7 id="26" title="Contact form 1"]' ); ?>
-
-    </div>
-
+	<div class="rhd-container">
+	<h1>Get in touch</h1>
+	<p>Need some brand help? Lets talk.</p>
+	<?php echo do_shortcode( '[contact-form-7 id="26" title="Contact form 1"]' ); ?>
+	</div>
 </section>
--- theme/template-work.php
+++ theme/template-work.php
@@ -1,35 +1,36 @@
 <?php /* Template Name: Work */?>
 
-
 <?php get_header(); ?>
 <main class="rhd-main">
-  <section>
+  <section class="rhd-work-example-banner">
   <div class="rhd-container">
      <div class="rhd-row flex flex-row flex-wrap flex-hor-center">
       <div class="rhd-work-heading"><h1>navigreat fine foods</h1></div>
-      <div class="rhd-view-more-work"><a href="" title=""><i class="fas fa-chevron-left"></i></a><span>view more work</span> <a href="" title=""><i class="fas fa-chevron-right"></i></a></div>
+      <div class="rhd-view-more-work"><a href="" title=""><i class="fas fa-chevron-left"></i></a> <span>view more work</span>  <a href="" title=""><i class="fas fa-chevron-right"></i></a></div>
      </div>
      <img src="<?php echo bloginfo('template_url'); ?>/img/navigreat-hero.png" alt="">
   </div>
   </section>
-  <section class="rhd-container">
+  <section class="rhd-container rhd-work-example-info">
     <div class="rhd-row flex flex-row flex-wrap flex-hor-center">
-    <aside class="rhd-aside">
+    <aside class="rhd-aside brand-description">
       <h4>Description</h4>
-      <p>Brand Identity</p>
+      <p>Brand Naming, Brand Identity, Brand Strategy</p>
       <h4>Client Website</h4>
       <p><a href="" title="">navigreatfood.com</a></p>
     </aside>
-    <div class="rhd-content-right">
-      <img src="<?php echo bloginfo('template_url'); ?>/img/" alt="">
+    <div class="rhd-work-example-logo">
+      <img src="<?php echo bloginfo('template_url'); ?>/img/navigreat.svg" alt="">
+    </div>
+    <div class="rhd-content-right brand-description">
       <p>This Arizona-based client needed to find a new brand name and identity that identified the evolution of their business that had significantly changed since their inception. Navigreat was a name Robin developed along with the tagline ‘Finding new taste territories’ that worked for the dual audience of both foodpreneurs and food retailers. The logo and ‘food map’ helped bring whimsy and direction, clearly delineating the brand position.</p>
     </div>
    </div>
   </section>
   <section class="rhd-section-light-grey">
     <div class="rhd-container">
-      <p class="u-italic u-text-center">“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget commodo augue, vel molestie ipsum. Vestibulum lacus neque, aliquam vel finibus vitae, posuere tincidunt dolor. Vivamus vitae risus mauris. Nam blandit nunc lectus, sollicitudin lobortis ipsum fringilla ac.”</p>
-      <p class="u-text-center"><strong >First & Last Name</strong><br>Job Position/Title</p>
+      <p class="u-italic u-text-center">“There are so many consumer food businesses in the U.S. and we wanted to avoid any trademark problems as well as distinguish ourselves as unique. The other important element was that we have several different audiences - the foodpreneurs we help - and the manufacturers and retailers we distribute to. The brand Robin created for us has worked equally well for all our customers and has the humour that breaks through the clutter.”</p>
+      <p class="u-text-center"><strong >Greg Bruni</strong><br>President of Navigreat Fine Food Co.</p>
     </div>
   </section>
   <?php include 'contact.php'; ?>
